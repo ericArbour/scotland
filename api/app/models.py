e266491ffea09b1f88051a9c1dfd11791a98a62b
Eric Arbour <arboureric@gmail.com>
2023-11-26 10:22:31 -0500

    Add models and migration

--- api/app/models.py
+++ api/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Distillery(models.Model):
+    name = models.CharField(max_length=25)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Whisky(models.Model):
+    name = models.CharField(max_length=50)
+    rating = models.IntegerField(choices=[(i, i) for i in range(101)])
+    distillery = models.ForeignKey(
+        Distillery, on_delete=models.CASCADE, related_name="whiskies"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
